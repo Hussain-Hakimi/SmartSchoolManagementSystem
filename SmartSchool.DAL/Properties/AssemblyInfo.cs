45eb0a649e9ff280a82ab1e958efb295851a923b
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-05-08 23:52:46 +0500

    add three layer business logic layer , data layer.... i will start my  project tomorrow

--- SmartSchool.DAL/Properties/AssemblyInfo.cs
+++ SmartSchool.DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmartSchool.UI")]
+[assembly: AssemblyTitle("SmartSchool.DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SmartSchool.UI")]
+[assembly: AssemblyProduct("SmartSchool.DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a4d11b19-24e3-4a0a-873b-1074ca140237")]
+[assembly: Guid("76100456-8e79-4c94-a631-702db60538b3")]
 
 // Version information for an assembly consists of the following four values:
 //
